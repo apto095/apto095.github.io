9753d6e00df3ed68ae97861c2716b63c6735668e
apto095 <ricardodarkar16@gmail.com>
2025-04-01 20:55:24 -0600

    0.2
    Implementación de página principal

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const capaInicio = document.getElementById('capa-inicio');
+  const contenidoPrincipal = document.getElementById('contenido-principal');
+
+  // Oculta la capa de inicio al hacer clic o tocar
+  capaInicio.addEventListener('click', function() {
+      // Animación de desvanecimiento
+      capaInicio.style.opacity = '0';
+      
+      // Espera a que termine la transición para ocultar
+      setTimeout(() => {
+          capaInicio.classList.add('capa-oculta');
+          contenidoPrincipal.classList.add('contenido-visible');
+      }, 500); // 0.5 segundos (debe coincidir con la transición CSS)
+  });
+});
+
 const tl = gsap.timeline({
   defaults:{
     duration:2,
